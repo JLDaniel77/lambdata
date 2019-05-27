bd6cf5cce21381e52260366173d15d1ff8fff7a9
Jeff Daniel <jeff.daniel77@gmail.com>
2019-05-27 15:23:57 -0400

    Adding initial POC package/module

---
+++ lambdata_jldaniel77/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+lambdata - a collection of Data Science helper functions
+"""
+
+import pandas as pd
+import numpy as np 
+from . import example_module
+
+Y = example_module.increment(example_module.x)
+TEST = pd.DataFrame(np.ones(10))
+
+# For later use to disable autocomplete:
+# "editor.acceptSuggestionOnCommitCharacter": false
